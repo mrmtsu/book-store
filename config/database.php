264565cb7b5e7203e8de58fa4267e1d6758638a4
mrmtsu <81202muramatsu@gmail.com>
2020-05-06 11:06:07 +0900

    edit_circleci9

--- config/database.php
+++ config/database.php
@@ -96,7 +96,7 @@ return [
             'host' => '127.0.0.1',
             'port' => '5432',
             'database' => 'circle_test',
-            'username' => 'default',
+            'username' => 'root',
             'password' => 'secret',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
